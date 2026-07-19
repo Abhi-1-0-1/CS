1a1a215f707f8821a2d2c4a2713f7714c18d325a
unknown <abhinavmparasher@gmail.com>
2026-07-19 17:27:00 +0400

    fix: change BrowserRouter to HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cs-notes/', // ⚠️ Must match your exact repository name on GitHub!
+  base: '/CS/', 
 })
